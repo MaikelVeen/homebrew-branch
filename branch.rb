7f20137be14ee8bf440701861de43239b1fba649
Maikel Veen <veenmaikel@gmail.com>
2022-01-14 14:07:21 +0100

    version 0.0.1

---
+++ branch.rb
@@ -0,0 +1,32 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Branch < Formula
+    desc "Branch is a small CLI tool to automatically create git branches based on tickets"
+    homepage "https://github.com/MaikelVeen/branch"
+    url "https://github.com/MaikelVeen/branch/archive/refs/tags/v0.0.1.tar.gz"
+    sha256 "700b7a7f3ca198e1de81cdbacbcac2e1bc29140cc2e32855818563fc4ba697f4"
+    license "MIT"
+  
+    # depends_on "cmake" => :build
+  
+    depends_on "go" => :build
+
+    def install
+      system "go", "build", *std_go_args(ldflags: "-s -w"), "./main.go"
+    end
+  
+    test do
+      # `test do` will create, run in and delete a temporary directory.
+      #
+      # This test will fail and we won't accept that! For Homebrew/homebrew-core
+      # this will need to be a test that verifies the functionality of the
+      # software. Run the test with `brew test branch`. Options passed
+      # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+      #
+      # The installed folder is not in the path, so use the entire path to any
+      # executables being tested: `system "#{bin}/program", "do", "something"`.
+      system "false"
+    end
+  end
+  
